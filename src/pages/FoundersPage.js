6ca4d2e9b376263b7ff53211e23ca7c430381b90
githubHemanth <60767478+githubHemanth@users.noreply.github.com>
2025-08-07 17:34:25 -0700

    Formatting changes

--- src/pages/FoundersPage.js
+++ src/pages/FoundersPage.js
@@ -20,13 +20,13 @@ function FoundersPage() {
             from India and creating a space designed to nurture peace, joy, and devotion in every visitor.</p>
             <h5>A Journey of Welcome and Belonging</h5>
           <p class="p2">The moment you step into the Yoga Hanuman Temple, you are greeted not just as a guest, but as family. Our dedicated volunteers
-             welcome you with warmth, kindness, and an open heart—ensuring everyone feels at home, no matter their background or experience. This spirit
+             welcome you with warmth, kindness, and an open heart, ensuring everyone feels at home, no matter their background or experience. This spirit
               of inclusivity is the cornerstone of what we do.</p>
           <p class="p2">Panditji’s pooja ceremonies are renowned for theirdevotion, musicality, and soul-stirring energy, reminiscent of the great South 
             Indian temples. His vibrant hymns, passionate chanting, and unwavering attention to detail transport devotees, inspiring a deep connection 
             with Hanumanji.</p>
             <h5>Embracing Peace and Culture</h5>
-          <p class="p2">Our sanctuary is more than a place to pray—it is a space to find inner peace and calm. When services are not in session, we invite
+          <p class="p2">Our sanctuary is more than a place to pray - it is a space to find inner peace and calm. When services are not in session, we invite
              everyone to take a moment to sit quietly, absorb the serene surroundings, and chant Anchaneya’s name in personal reflection. Families, especially
               parents with young children, are encouraged to cultivate mindful silence and instill peaceful habits and spiritual values from an early age.</p>
               <h5>Building Our Future, Together</h5>
